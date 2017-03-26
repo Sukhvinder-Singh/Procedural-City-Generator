23f83d52836593b5d363d806b5a0b1137a694222
Sukhvinder <sukhvisukh@gmail.com>
2017-03-26 09:06:23 +0530

    Add files via upload

---
+++ CityGen.cs
@@ -0,0 +1,200 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class CityGen : EditorWindow
+{
+
+
+	//Data about modes
+
+	public bool totallyRandom;
+	public bool pointMode;
+
+	//Data about primitive
+	public int height;
+	public int width;
+
+	//Data about building to be generated
+	public int floors;
+	public int minfloor;
+	public bool randomFloors;
+	public int xpos;
+	public int zpos;
+
+	//Data about building generation type
+
+	public bool manygen;
+	public int XrandomRange;
+	public int ZrandomRange;
+	public int iter;
+
+	//Data about generation ar points
+	public GameObject transformPos;
+	public GameObject[] primitives;
+	public bool rot;
+	public bool placer;
+
+	int f;
+
+	[MenuItem("CityGen/Procedural City Generation tool")]
+
+	static void Init() {
+		EditorWindow window = GetWindow(typeof(CityGen));
+		window.Show();
+	}
+
+	void OnGUI() {
+
+
+
+		EditorGUILayout.BeginHorizontal();
+		totallyRandom = EditorGUILayout.Toggle ("random placement mode ", totallyRandom);
+		pointMode = EditorGUILayout.Toggle ("Point mode placement", pointMode);
+
+		EditorGUILayout.EndHorizontal ();
+
+		EditorGUILayout.BeginHorizontal();
+		height = EditorGUILayout.IntField ("Height of primitive: ", height);
+		width = EditorGUILayout.IntField ("width of primitive:", width);
+
+		EditorGUILayout.EndHorizontal ();
+
+
+		EditorGUILayout.BeginHorizontal();
+
+		floors = EditorGUILayout.IntField ("max floors: ", floors);
+		minfloor = EditorGUILayout.IntField ("min floors:", minfloor); 
+		xpos = EditorGUILayout.IntField ("Instantiate at X position :", xpos);
+		zpos = EditorGUILayout.IntField ("Instantiate at Z position :", zpos);
+		randomFloors = EditorGUILayout.Toggle ("Random floors?", randomFloors);
+
+		EditorGUILayout.EndHorizontal ();
+
+		EditorGUILayout.BeginHorizontal();
+
+		manygen = EditorGUILayout.Toggle ("Generate many buildings??", manygen);
+		XrandomRange = EditorGUILayout.IntField ("X axis randomness:", XrandomRange);
+		ZrandomRange = EditorGUILayout.IntField ("Z axis randomness:", ZrandomRange);
+		iter = EditorGUILayout.IntField ("iterations:", iter);
+
+		EditorGUILayout.EndHorizontal ();
+
+
+
+		EditorGUILayout.BeginHorizontal();
+
+		placer = EditorGUILayout.Toggle ("Generate at points:", placer);
+		rot = EditorGUILayout.Toggle ("Allow random rotation:", rot);
+		transformPos = EditorGUILayout.ObjectField ( "Transform object", transformPos, typeof(GameObject)) as GameObject;
+
+
+		ScriptableObject target = this;
+		SerializedObject so = new SerializedObject(target);
+		SerializedProperty stringsProperty = so.FindProperty("primitives");
+
+		EditorGUILayout.PropertyField(stringsProperty, true); // True means show children
+		so.ApplyModifiedProperties(); // Remember to apply modified properties
+
+
+		EditorGUILayout.EndHorizontal ();
+
+
+
+		if (GUILayout.Button("Start generation")) {
+
+			if (totallyRandom) {
+				if (!manygen) {
+					if (!randomFloors) {
+						int counter = 0;
+						for (int x = 0; x < floors; x++) {
+
+							GameObject go = PrefabUtility.InstantiatePrefab (Selection.activeObject as GameObject) as GameObject; 
+
+							go.transform.position = new Vector3 (xpos, counter + height, 0);
+							counter += height;
+						}
+					} else if (randomFloors) {
+						int counter = 0;
+						for (int x = 0; x < Random.Range (minfloor, floors); x++) {
+
+							GameObject go = PrefabUtility.InstantiatePrefab (Selection.activeObject as GameObject) as GameObject; 
+
+							go.transform.position = new Vector3 (xpos, counter + height, zpos);
+							counter += height;
+						}
+					}
+				
+				} else if (manygen) {
+					for (int c = 0; c < iter; c++) {
+						int xr = Random.Range (xpos, XrandomRange);
+						int yr = Random.Range (zpos, ZrandomRange);
+
+						int counter = 0;
+
+
+						if (randomFloors)
+							f = Random.Range (minfloor, floors);
+						else if (!randomFloors)
+							f = floors;
+						for (int x = 0; x < f; x++) {
+
+							GameObject go = PrefabUtility.InstantiatePrefab (Selection.activeObject as GameObject) as GameObject; 
+
+							go.transform.position = new Vector3 (xr, counter + height, yr);
+							counter += height;
+						}
+
+					
+					}
+				}
+			}
+
+			else if(pointMode) {
+
+
+			
+
+					foreach (Transform child in transformPos.transform) {
+
+					if (!randomFloors) {
+
+						GameObject go = PrefabUtility.InstantiatePrefab (primitives [Random.Range (0, primitives.Length)] as GameObject) as GameObject; 
+
+						go.transform.position = child.transform.position;
+
+						if(rot) {
+							go.transform.rotation = Quaternion.Euler (0, Random.Range (10, 90), 0);
+						}
+
+					}
+
+					else if(randomFloors) {
+
+					
+							int counter = 0;
+						int ff = Random.Range (0, primitives.Length);
+							for (int x = 0; x < Random.Range(minfloor,floors); x++) {
+
+							GameObject go = PrefabUtility.InstantiatePrefab (primitives [ff] as GameObject) as GameObject; 
+
+							go.transform.position = new Vector3 (child.transform.position.x, counter + height, child.transform.position.z);
+								counter += height;
+							}
+
+					}
+
+				
+					}
+
+			}
+			
+		}
+
+	}
+
+
+
+
+
+}
---
+++ pointsGen.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class pointsGen:EditorWindow {
+
+	//data about point type
+	public int npoints; 
+	public bool squaregrid;
+	public bool randomPlacement;
+	public bool lineup;
+	public int squareDis;
+	public int minDisX;
+	public int MaxDisX;
+	public int minDisZ;
+	public int MaxDisZ;
+	public bool uniformdis; 
+
+
+	[MenuItem("CityGen/Points Placer")]
+
+	static void Init() {
+		EditorWindow window = GetWindow(typeof(pointsGen));
+		window.Show();
+	}
+
+
+	void OnGUI() {
+		EditorGUILayout.BeginHorizontal ();
+		npoints = EditorGUILayout.IntField ("array size?", npoints); 
+		squaregrid = EditorGUILayout.Toggle ("Create square grid?",squaregrid);
+		randomPlacement = EditorGUILayout.Toggle ("Random placement?", randomPlacement);
+		lineup = EditorGUILayout.Toggle ("building lineup", lineup);
+		EditorGUILayout.EndHorizontal ();
+
+		if(squaregrid)
+		{
+			EditorGUILayout.BeginHorizontal ();
+			squareDis = EditorGUILayout.IntField ("distance between", squareDis); 
+			EditorGUILayout.EndHorizontal ();
+
+			if(GUILayout.Button("Generate array")) {
+
+				int xdis = 0, zdis = 0;
+
+				for (int i = 0; i < npoints; i++)
+				{
+
+					for(int j=0;j<npoints;j++) {
+						GameObject pt = new GameObject ("pt");
+						zdis += squareDis;
+						pt.transform.position = new Vector3 (xdis, 0, zdis); 
+					}
+					zdis = 0;
+					xdis += squareDis;
+				}
+
+
+			}
+
+
+			
+		}
+
+		else if(randomPlacement)
+		{
+			EditorGUILayout.BeginHorizontal ();
+			minDisX = EditorGUILayout.IntField ("min x distance", minDisX); 
+			MaxDisX = EditorGUILayout.IntField ("max x distance", MaxDisX);
+
+			EditorGUILayout.EndHorizontal ();
+
+			EditorGUILayout.BeginHorizontal ();
+			minDisZ = EditorGUILayout.IntField ("min z distance", minDisZ); 
+			MaxDisZ = EditorGUILayout.IntField ("max z distance", MaxDisZ);
+
+			EditorGUILayout.EndHorizontal ();
+
+
+			if(GUILayout.Button("Generate random")) {
+				//do things
+
+				int zrdis=0, xrdis=0;
+
+				for (int i = 0; i < npoints; i++)
+				{
+
+					for(int j=0;j<npoints;j++) {
+						GameObject pt = new GameObject ("pt");
+						xrdis = Random.Range (minDisX, MaxDisX) ;
+						pt.transform.position = new Vector3 (xrdis,0,Random.Range(minDisZ,MaxDisZ)); 
+					}
+
+					minDisX += Random.Range (minDisX+10, minDisX*2);
+					MaxDisX += Random.Range (MaxDisX+10, MaxDisX*2);
+	
+				}
+
+
+
+			}
+
+		}
+
+
+
+		else if(lineup) {
+
+
+			EditorGUILayout.BeginHorizontal ();
+			uniformdis = EditorGUILayout.Toggle ("Uniform distance?",uniformdis);
+			EditorGUILayout.EndHorizontal ();
+
+
+			if(uniformdis) {
+
+				EditorGUILayout.BeginHorizontal ();
+				squareDis = EditorGUILayout.IntField ("distance?",squareDis);
+				EditorGUILayout.EndHorizontal ();
+
+
+				if(GUILayout.Button("generate lineup")) {
+
+					for(int a=0;a<npoints;a++) {
+						GameObject g = new GameObject ("pt");
+						g.transform.position = new Vector3 (0, 0, a + squareDis);
+					}
+				}
+
+
+			}
+
+
+			if(!uniformdis) {
+
+				EditorGUILayout.BeginHorizontal ();
+				minDisZ = EditorGUILayout.IntField ("min distance?",minDisZ);
+				MaxDisZ = EditorGUILayout.IntField ("max distance?",MaxDisZ);
+				EditorGUILayout.EndHorizontal ();
+
+
+				if(GUILayout.Button("generate lineup")) {
+
+					for(int a=0;a<npoints;a++) {
+						GameObject g = new GameObject ("pt");
+						g.transform.position = new Vector3 (0, 0, a + Random.Range(minDisZ,MaxDisZ));
+					}
+				}
+
+
+			}
+
+			
+		}
+
+
+
+
+
+	}
+
+
+}
